91dbf028083c20f2456773b5544f83a55fc896d4
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-21 00:33:03 -0300

    add migration createOrganizers

---
+++ src/database/migrations/1647830279154-CreateOrganizers.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner,Table} from "typeorm";
+
+export class CreateOrganizers1647830279154 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+              name: "organizers",
+              columns: [
+                {
+                  name: "id",
+                  type: "uuid",
+                },
+                {
+                  name: "name",
+                  type: "varchar",
+                },
+                {
+                  name: "email",
+                  type: "varchar",
+                  isUnique: true,
+                },
+                {
+                  name: "password",
+                  type: "varchar",
+                },
+                {
+                  name: "phone_number",
+                  type: "int",
+                },
+                {
+                  name: "business_type",
+                  type: "varchar",
+                },
+                {
+                  name: "cnpj",
+                  type: "varchar",
+                },
+                {
+                    name: "corporate_name",
+                    type: "varchar",
+                },
+                {
+                  name: "created_at",
+                  type: "timestamp",
+                  default: "now()",
+                },
+              ],
+            })
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

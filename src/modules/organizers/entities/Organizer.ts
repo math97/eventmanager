2041e651dd90fc7b65dececd8a8f4cdaf2cf6534
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-21 00:42:43 -0300

    add tags of typeorm on entity

--- src/modules/organizers/entities/Organizer.ts
+++ src/modules/organizers/entities/Organizer.ts
@@ -1,13 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 class Organizer {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @Column()
   name: string;
-  phone_number: string;
+
+  @Column("business_type")
   businessType: string;
+
+  @Column()
   cnpj: string;
+
+  @Column("corporate_name")
   corporateName: string;
+
+  @Column("phone_number")
   phoneNumber: Number;
+
+  @CreateDateColumn()
+  created_at: Date;
 }
 
 export { Organizer }

9ecbfcc5140355ae32b399890d27f7f48ea9fced
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-20 23:07:16 -0300

    add Organizer Entity

---
+++ src/modules/organizers/entities/Organizer.ts
@@ -0,0 +1,13 @@
+class Organizer {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  phone_number: string;
+  businessType: string;
+  cnpj: string;
+  corporateName: string;
+  phoneNumber: Number;
+}
+
+export { Organizer }

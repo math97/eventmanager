68dcd8f9371b74099d5e9f0c630fe6e7f0203ed0
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-22 22:06:49 -0300

    add userAuthentication

---
+++ src/modules/users/useCases/authenticateUserUseCase/AuthenticateUserUseCase.ts
@@ -0,0 +1,50 @@
+import { inject, injectable } from "tsyringe";
+import { sign } from "jsonwebtoken";
+import { compare } from "bcryptjs";
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+interface IRequest {
+  email:string,
+  password:string,
+}
+
+interface IResponse {
+  user: {
+    email: string;
+    name: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository:IUserRepository){ }
+  
+  async execute({email,password}:IRequest):Promise<IResponse>{
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) throw new Error("Email or password incorrect!");
+
+    const passwordMatch = await compare(password,user.password);
+
+    if (!passwordMatch) throw new Error("Email or password incorrect!");
+
+    const token = sign({}, "179d1e5dfc5ac8ea3e4f0d4887890e0e", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+    return tokenReturn;
+  }
+}
+
+export {AuthenticateUserUseCase};
--- src/routes/authenticate.routes.ts
+++ src/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import {AuthenticateOrganizerController  } from "../modules/organizers/useCases/authenticateOrganizersUseCase/AuthenticateOrganizerController";
+import {AuthenticateUserController  } from "../modules/users/useCases/authenticateUserUseCase/AuthenticateUserController";
 
 const authenticateRoutes = Router();
 
 const authenticateOrganizerController = new AuthenticateOrganizerController();
+const authenticateUserController = new AuthenticateUserController();
 
 authenticateRoutes.post("/organizer", authenticateOrganizerController.handle);
+authenticateRoutes.post("/user", authenticateUserController.handle);
 
 export { authenticateRoutes };

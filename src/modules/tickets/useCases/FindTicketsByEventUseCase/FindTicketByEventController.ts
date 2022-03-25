425a39acf4eea97d376aca847340a4d3c53eddde
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-03-25 16:46:01 -0300

    add errorhandler on findByTicketByEventUseCase

--- src/modules/tickets/useCases/FindTicketsByEventUseCase/FindTicketByEventController.ts
+++ src/modules/tickets/useCases/FindTicketsByEventUseCase/FindTicketByEventController.ts
@@ -1,19 +1,23 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { container } from "tsyringe";
+import AppError from "../../../../errors/AppError";
 import { FindTicketsByEventUseCase } from "./FindTicketByEventUseCase";
 
 class FindTicketsByEventController{
-  async handle(request:Request,response:Response):Promise<Response>{
-    const findTicketsByEventUseCase = container.resolve(FindTicketsByEventUseCase);
+  async handle(request:Request,response:Response,next:NextFunction):Promise<Response | undefined>{
+    try {
+      const findTicketsByEventUseCase = container.resolve(FindTicketsByEventUseCase);
     
-    const {eventId} = request.query;
-    
-    if(!eventId || typeof eventId != "string") throw new Error("eventId not sent");
+      const {eventId} = request.query;
+      
+      if(!eventId || typeof eventId != "string") throw new AppError("eventId not sent or invalid",400);
 
-    const tickets = await findTicketsByEventUseCase.execute(eventId);
+      const tickets = await findTicketsByEventUseCase.execute(eventId);
 
-    return response.json(tickets);
+      return response.json(tickets);
+    } catch (error) {
+      next(error);
+    }
   }
 };
-
 export {FindTicketsByEventController}
--- src/modules/tickets/useCases/FindTicketsByEventUseCase/FindTicketByEventUseCase.ts
+++ src/modules/tickets/useCases/FindTicketsByEventUseCase/FindTicketByEventUseCase.ts
@@ -20,5 +20,4 @@ class FindTicketsByEventUseCase{
       return tickets;
     }
 };
-
 export {FindTicketsByEventUseCase}
